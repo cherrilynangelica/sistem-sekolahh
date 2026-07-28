3b455d0dffcf4d68443cfb11fe7b40e55910a325
cherrilynangelica <cherrilynangelica@gmail.com>
2026-07-28 14:07:14 +0700

    Menambahkan jurusan dan kelas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\SchoolClass\CreateController;
+use App\Http\Controllers\SchoolClass\DestroyController;
+use App\Http\Controllers\SchoolClass\EditController;
+use App\Http\Controllers\SchoolClass\IndexController;
+use App\Http\Controllers\SchoolClass\ShowController;
+use App\Http\Controllers\SchoolClass\StoreController;
+use App\Http\Controllers\SchoolClass\UpdateController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\MajorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -39,3 +47,21 @@ Route::name('teachers.')->prefix('teachers')->group(function () {
 
     Route::delete('/{id}', [TeacherController::class, 'destroy'])->name('destroy');
 });
+
+Route::name('schoolclass.')->prefix('schoolclass')->group(function () {
+    Route::get('/', IndexController::class)->name('index');
+
+    Route::get('/create', CreateController::class)->name('create');
+
+    Route::get('/{id}', ShowController::class)->name('show');
+
+    Route::get('/{id}/edit', EditController::class)->name('edit');
+
+    Route::post('/', StoreController::class)->name('store');
+
+    Route::put('/{id}', UpdateController::class)->name('update');
+
+    Route::delete('/{id}', DestroyController::class)->name('destroy');
+});
+
+Route::resource('majors', MajorController::class);
